27df7e47c51ddf4ebfc0a0c3a982676b54fd8d10
SoraOrihara <caca66z@gmail.com>
2025-10-03 13:06:48 -0300

    Fazendo o user com UserDetails, service UserDetails, configurando o
    FilterChain com o passWordEncoder

---
+++ src/main/java/br/com/springEstudo/SpringSecurity/configs/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package br.com.springEstudo.SpringSecurity.configs.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.springEstudo.SpringSecurity.infraestructure.models.UserModel;
+import br.com.springEstudo.SpringSecurity.infraestructure.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	final UserRepository userRepository;
+	public UserDetailsServiceImpl(UserRepository userRepository) {
+		this.userRepository=userRepository;
+	}
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		UserModel userModel= userRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("Username not found: "+username));
+		return userModel; 
+	}
+
+}
